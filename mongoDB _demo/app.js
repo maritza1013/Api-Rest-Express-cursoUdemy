d5fd21d528449b97a815ae1eea93af69d194ce14
maritza1013 <maritzamae13@gmail.com>
2023-08-09 18:41:07 -0500

    esquemas y modelos

--- mongoDB _demo/app.js
+++ mongoDB _demo/app.js
@@ -3,3 +3,35 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/demo', {useNewUrlParser:true, useUnifiedTopology:true})
     .then(() => console.log('conectado a mongodb...'))
     .catch(()=> console.log('no se pudo conectar con mongodb...', err))
+
+    const cursoSchema = new mongoose.Schema({
+        nombre      : String,
+        autor       : String,
+        etiquetas   : [String],
+        fecha       : { type:Date, default:Date.now},
+        publicado   : Boolean
+    });
+
+    const Curso = mongoose.model('curso', cursoSchema);
+
+    async function crearCurso(){
+        const curso = new Curso ({
+            nombre:'Angular para principiantes',
+            autor:'maria ruiz',
+            etiquetas:['desarrollo web','front end'],
+            publicado:true
+        })
+        const resultado = await curso.save()
+        console.log(resultado)
+    };
+    // crearCurso();
+
+    async function listarCursos(){
+        const cursos = await Curso
+        .find({publicado:true})
+        .limit(10)
+        .sort({autor:1})
+        .select({nombre:1, etiquetas:1})
+        console.log(cursos);
+    }
+    listarCursos();
